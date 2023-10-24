4063906d7ffeb0542a7935369ccc8bb26462dd7f
Oumi102000 <o.elmadani10@gmail.com>
2023-10-24 22:23:38 +0000

    Update User class for BDstorage

--- models/user.py
+++ models/user.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """This class defines a user by various attributes
+
+    Attributes:
+        __tablename__ (str): MySQL's table name to store users
+        email (String): user's email adress.
+        password (String): user's password.
+        first_name (String): user's first name.
+        last_name (String): user's last name.
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
