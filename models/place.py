a0ff281ef82b0bc37672d935e8c407d5e9601f9c
Hajar Zaitouni <hajarzaitouni88@gmail.com>
2023-10-25 17:37:09 -0700

    Update Amenity and Place class

--- models/place.py
+++ models/place.py
@@ -2,10 +2,19 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, Table
 from sqlalchemy.orm import relationship
 from models.review import Review
+from models.amenity import Amenity
 from os import getenv
+
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column("place_id", String(60), ForeignKey("places.id"),
+                             primary_key=True, nullable=False),
+                      Column("amenity_id", String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -38,6 +47,8 @@ class Place(BaseModel, Base):
     amenity_ids = []
 
     reviews = relationship("Review", backref="place", cascade="delete")
+    amenities = relationship("Amenity", secondary="place_amenity",
+                             viewonly=False)
 
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
@@ -52,3 +63,23 @@ class Place(BaseModel, Base):
                 if rev.place_id == self.id:
                     my_reviews.append(rev)
             return my_reviews
+
+        @property
+        def amenities(self):
+            """
+            get the list of Amenities linked to place
+            """
+            my_amenities = []
+            all_instan_amenities = list(models.storage.all(Amenity).values())
+            for amenity in all_instan_amenities:
+                if amenity.id in amenity_ids:
+                    my_amenities.append(amenity)
+            return my_amenities
+
+        @setter.amenities
+        def amenities(self, value):
+            """set amenities that handles append method for adding
+            an Amenity.id to the attribute amenity_ids
+            """
+            if isinstance(value, "Amenity"):
+                amenity_ids.append(value.id)
