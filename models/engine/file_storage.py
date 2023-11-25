958407ca9835185b3df1f002379b9037eb4327b9
Hajar Zaitouni <hajarzaitouni88@gmail.com>
2023-11-25 15:13:01 +0000

    Update db and file storage with close function

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -66,3 +66,7 @@ class FileStorage:
         if key in self.__objects:
             del self.__objects[key]
             self.save()
+
+    def close(self):
+        """ call reload() method for deserializing the JSON file to objects """
+        self.reload()
